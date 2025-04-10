d31044709e578422aba62ef0e674726387daa637
Anastasiia <enfenim.dinan@gmail.com>
2025-04-10 21:45:52 +0200

    Initial Commit

---
+++ Guess the Flag/Guess_the_FlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  Guess_the_FlagApp.swift
+//  Guess the Flag
+//
+//  Created by Anastasiia Kotova on 10.04.25.
+//
+
+import SwiftUI
+
+@main
+struct Guess_the_FlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
